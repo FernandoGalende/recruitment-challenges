4b235a7e0cf2be4e59c1e8b5b28da710bde319d2
Fernando Aparicio Galende <f.aparicio@Payvision.local>
2018-06-17 18:20:58 +0200

    refactor 2.0

--- Normalize/NomalizeEmail.js
+++ Normalize/NomalizeEmail.js
@@ -1,8 +1,6 @@
 const normalizeEmail = email => {
     if (email.indexOf("+") > 0) {
-      email =
-        email.substring(0, email.indexOf("+")) +
-        email.substring(email.indexOf("@"), email.length);
+      email = email.substring(0, email.indexOf("+")) + email.substring(email.indexOf("@"), email.length);
     }
     return email.replace(/[\.]/i, "");
   };
--- ReadFraudLines.js
+++ ReadFraudLines.js
@@ -4,6 +4,16 @@ const normalize = require("./Normalize/Normailze");
 let readFraudLines = filePath => {
   let orders = [];
   let fileContent;
+  const keys = [
+    'orderId',
+    'dealId',
+    'email',
+    'street',
+    'city',
+    'state',
+    'zipCode',
+    'creditCard'
+  ];
 
   try {
     fileContent = fs.readFileSync(filePath, "utf8");
@@ -13,32 +23,33 @@ let readFraudLines = filePath => {
 
   class Order {
     constructor(items) {
-      (this.orderId = items[0]),
-        (this.dealId = items[1]),
-        (this.email = items[2]),
-        (this.street = items[3]),
-        (this.city = items[4]),
-        (this.state = items[5]),
-        (this.zipCode = items[6]),
-        (this.creditCard = items[7]);
+      for (let i = 0; i < keys.length; i++) {
+        this[keys[i]] = items[i];
+      }
     }
 
     format() {
-      (this.orderId = Number(this.orderId)),
-        (this.dealId = Number(this.dealId)),
-        (this.email = this.email.toLowerCase()),
-        (this.street = this.street.toLowerCase()),
-        (this.city = this.city.toLowerCase()),
-        (this.state = this.state.toLowerCase());
+      for (let i = 0; i <= keys.indexOf("dealId"); i++) {
+        this[keys[i]] = Number(this[keys[i]]);
+      }
+      for (let j = keys.indexOf("email"); j <= keys.indexOf("state"); j++) {
+        this[keys[j]] = this[keys[j]].toLowerCase();
+      }
       return this;
     }
-   }
+  }
 
   let lines = fileContent.split("\n");
   for (let line of lines) {
     let items = line.split(",");
     let myOrder = new Order(items).format();
-    myOrder.email, myOrder.street, myOrder.state = normalize.normalize( myOrder.email, myOrder.street, myOrder.state );
+    myOrder.email,
+      myOrder.street,
+      (myOrder.state = normalize.normalize(
+        myOrder.email,
+        myOrder.street,
+        myOrder.state
+      ));
     orders.push(myOrder);
   }
 
